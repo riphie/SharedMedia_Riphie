212ca236f39d78dbe53e61da93672d6460f18087
Tom Bell <tomb@tomb.io>
2023-02-26 21:23:28 +0000

    Add core.lua

---
+++ core.lua
@@ -0,0 +1,27 @@
+local A, L = ...
+
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local mediapath = "Interface\\AddOns\\" .. A
+
+--
+-- FONTS
+--
+
+LSM:Register("font", "Expressway", mediapath .. "\\fonts\\Expressway.ttf")
+
+--
+-- STATUSBARS
+--
+
+LSM:Register("statusbar", "Skullflower", mediapath .. "\\statusbar\\Skullflower.tga")
+LSM:Register("statusbar", "Skullflower1", mediapath .. "\\statusbar\\Skullflower1.tga")
+LSM:Register("statusbar", "Skullflower2", mediapath .. "\\statusbar\\Skullflower2.tga")
+LSM:Register("statusbar", "Skullflower3", mediapath .. "\\statusbar\\Skullflower3.tga")
+LSM:Register("statusbar", "Skullflower4", mediapath .. "\\statusbar\\Skullflower4.tga")
+LSM:Register("statusbar", "Skullflower5", mediapath .. "\\statusbar\\Skullflower5.tga")
+LSM:Register("statusbar", "SkullflowerGradient", mediapath .. "\\statusbar\\SkullflowerGradient.tga")
+LSM:Register("statusbar", "SkullflowerGradient1", mediapath .. "\\statusbar\\SkullflowerGradient1.tga")
+LSM:Register("statusbar", "SkullflowerGradient2", mediapath .. "\\statusbar\\SkullflowerGradient2.tga")
+LSM:Register("statusbar", "SkullflowerLight", mediapath .. "\\statusbar\\SkullflowerLight.tga")
+LSM:Register("statusbar", "SkullflowerNeon", mediapath .. "\\statusbar\\SkullflowerNeon.tga")
